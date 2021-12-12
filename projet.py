ee1ce41c9fff0d29c782d26930aa02fe4c0ae4a5
Nourchene-bh <86303905+Nourchene-bh@users.noreply.github.com>
2021-12-12 21:27:52 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -5,12 +5,12 @@ import time
 
 GPIO.setmode(GPIO.BOARD) 
 
-def get_last_watered():
+def dernier_arossage():
     try:
-        f = open("last_watered.txt", "r")
+        f = open("Arrosage.txt", "r")
         return f.readline()
     except:
-        return "NEVER!"
+        return "jamais!"
       
 def get_status(pin = 8):
     GPIO.setup(pin, GPIO.IN) 
@@ -21,28 +21,34 @@ def init_output(pin):
     GPIO.output(pin, GPIO.LOW)
     GPIO.output(pin, GPIO.HIGH)
     
-def auto_water(delay = 5, pump_pin = 7, humi_pin = 8):
-    wc = 0
-    init_output(pump_pin)
+    
+    
+    
+    def pompe_on(pompe_pin = 7, delay = 1):
+    init_output(pompe_pin)
+    f = open("Arrosage.txt", "w")
+    f.write("Arrosage {}".format(datetime.datetime.now()))
+    f.close()
+    GPIO.output(pompe_pin, GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(pompe_pin, GPIO.HIGH)
+
+    
+def auto_water(delay = 5, pompe_pin = 7, humi_pin = 8):
+    NA = 0
+    init_output(pompe_pin)
    
     try:
-        while  wc < 10:
+        while  NA < 10:
             time.sleep(delay)
-            wet = get_status(pin = humi_pin) == 0
-            if not wet:
-                if wc < 5:
-                    pump_on(pump_pin, 1)
-                wc += 1
+            mouiller = get_status(pin = humi_pin) == 0
+            if not mouiller:
+                if NA < 5:
+                   pompe_on(pompe_pin, 1)
+                NA += 1
             else:
-                wc = 0
+                NA = 0
     except KeyboardInterrupt: 
         GPIO.cleanup() 
 
-def pump_on(pump_pin = 7, delay = 1):
-    init_output(pump_pin)
-    f = open("last_watered.txt", "w")
-    f.write("Last watered {}".format(datetime.datetime.now()))
-    f.close()
-    GPIO.output(pump_pin, GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(pump_pin, GPIO.HIGH)
+
